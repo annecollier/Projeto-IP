eb296c7344585e9518833b439f3b61ddea8d996c
Maria Clara Albuquerque Moura <mcam3@cin.ufpe.br>
2022-10-20 20:41:16 -0300

    mudando os coletáveis e adicionando contagem por fotos

--- Projeto-IP-Testes-Coletando Itens/BobGroup.py
+++
@@ -1,8 +0,0 @@
-import SpongeSprite
-import SpriteGroups
-import PlanctonSprite
-
-bob = SpongeSprite.Sponge()
-plancton = PlanctonSprite.Plancton()
-SpriteGroups.todas_sprites.add(bob)
-SpriteGroups.todas_sprites.add(plancton)
