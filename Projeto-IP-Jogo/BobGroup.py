03fcf4cbb12db0c9000afc01a68d9ee88ac5cc87
Ricardo <raf@cin.ufpe.br>
2022-11-05 00:01:06 -0300

    BobGroup em class

--- Projeto-IP-Jogo/BobGroup.py
+++ Projeto-IP-Jogo/BobGroup.py
@@ -8,6 +8,11 @@ import BobInstrucoes
 import BobInstrucoes2
 import BobInstrucoes3
 import PlanctonInstrucoes
+
+
+
+
+
 class Personagens():
     def __init__(self):
         self.bob = SpongeSprite.Sponge()
@@ -15,25 +20,37 @@ class Personagens():
         self.plancton2 = inimigo_2.Plancton_2()
         SpriteGroups.todas_sprites.add(self.bob)
         SpriteGroups.todas_sprites.add(self.plancton)
-        #return self.bob, self.plancton, self.plancton2
 
 
-def personagens_menu():
-    bob_menu = BobInstrucoes.BobMenu()
-    SpriteGroups.bob_menu.add(bob_menu)
-    return bob_menu
-def personagens_menu2():
-    bob_menu2 = BobInstrucoes2.BobMenu2()
-    SpriteGroups.personagensmenu2.add(bob_menu2)
-    return bob_menu2
-def personagens_menu3():
-    bob_menu3 = BobInstrucoes3.BobMenu3()
-    plancton_menu = PlanctonInstrucoes.PlanctonMenu()
-    SpriteGroups.personagensmenu3.add(bob_menu3)
-    SpriteGroups.personagensmenu3.add(plancton_menu)
-    return bob_menu3,plancton_menu
+class Personagens_menu():
+
+    def __init__(self):
+
+        self.bob_menu = Personagens_menu.personagens_menu(self)
+        self.bob_menu2 = Personagens_menu.personagens_menu2(self)
+        self.bob_menu3 = Personagens_menu.personagens_menu3(self)
+        self.plancton_menu = Personagens_menu.personagens_menu3(self)
+
+
+    def personagens_menu(self):
+        self.bob_menu = BobInstrucoes.BobMenu()
+        SpriteGroups.bob_menu.add(self.bob_menu)
+
+    def personagens_menu2(self):
+        self.bob_menu2 = BobInstrucoes2.BobMenu2()
+        SpriteGroups.personagensmenu2.add(self.bob_menu2)
+
+    def personagens_menu3(self):
+        self.bob_menu3 = BobInstrucoes3.BobMenu3()
+        self.plancton_menu = PlanctonInstrucoes.PlanctonMenu()
+        SpriteGroups.personagensmenu3.add(self.bob_menu3)
+        SpriteGroups.personagensmenu3.add(self.plancton_menu)
 
 personagens = Personagens()
-bob_menu = personagens_menu()
-bob_menu2 = personagens_menu2()
-bob_menu3,plancton_menu = personagens_menu3()
+menus = Personagens_menu()
+
+
+
+
+
+
